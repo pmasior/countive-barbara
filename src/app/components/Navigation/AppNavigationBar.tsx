b42a9d9982216001d79d3644388baec1a79d101b
Paweł Mąsior <62357939+pmasior@users.noreply.github.com>
2021-12-25 21:42:50 +0100

    Change showing AppNavigationBar elements

--- src/app/components/Navigation/AppNavigationBar.tsx
+++ src/app/components/Navigation/AppNavigationBar.tsx
@@ -1,5 +1,6 @@
 import MenuIcon from "@mui/icons-material/Menu";
 import AppBar from "@mui/material/AppBar";
+import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import React, { FC } from "react";
@@ -12,24 +13,44 @@ import { SearchField } from "./SearchField";
 const AppNavigationBar: FC<{
   handleDrawerToggleOnClick: () => void;
 }> = ({ handleDrawerToggleOnClick }) => {
+  const displayOnMobileScreen = {
+    display: { xs: "block", md: "none" },
+  };
+  const displayOnDesktopScreen = {
+    display: { xs: "none", md: "block" },
+  };
+
   return (
     <>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerToggleOnClick}
-            sx={{ marginRight: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <AppLogo variant={"h5"} color={"white"} />
-          {/* TODO: add flex-grow to AppLogo */}
-          <NavigationTabs />
-          <SearchField />
-          <BudgetMenu />
-          <ProfileMenu />
+          <Grid container columnSpacing={1} alignItems="center">
+            <Grid item sx={{ ...displayOnMobileScreen }}>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerToggleOnClick}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Grid>
+            <Grid item sx={{ ...displayOnDesktopScreen }}>
+              <AppLogo variant={"h5"} color={"white"} />
+            </Grid>
+            <Grid item sx={{ ...displayOnDesktopScreen }}>
+              <NavigationTabs />
+            </Grid>
+            <Grid item sx={{ flexGrow: 1 }} />
+            <Grid item>
+              <SearchField />
+            </Grid>
+            <Grid item>
+              <BudgetMenu />
+            </Grid>
+            <Grid item sx={{ ...displayOnDesktopScreen }}>
+              <ProfileMenu />
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
       <Toolbar />
