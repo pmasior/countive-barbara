57cd479603dba193bd5310350ac3aeb3b6836327
Paweł Mąsior <62357939+pmasior@users.noreply.github.com>
2021-12-30 00:10:46 +0100

    Remove log

--- backend/auth/getEmailForSession.ts
+++ backend/auth/getEmailForSession.ts
@@ -2,6 +2,5 @@ import { getSession, GetSessionParams } from "next-auth/react";
 
 export const getEmailForSession = async (context: GetSessionParams) => {
   const session = await getSession(context);
-  console.log(session);
   return session?.user?.email;
 };
