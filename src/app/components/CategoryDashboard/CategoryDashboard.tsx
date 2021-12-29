40b1a2d2572aed41de2a53907b79f1c9a538020e
Paweł Mąsior <62357939+pmasior@users.noreply.github.com>
2021-12-29 16:16:15 +0100

    Remove debug

--- src/app/components/CategoryDashboard/CategoryDashboard.tsx
+++ src/app/components/CategoryDashboard/CategoryDashboard.tsx
@@ -9,13 +9,8 @@ export const CategoryDashboard = () => {
     <Grid container spacing={4} padding={4} minHeight="100%">
       <Grid item xs={12} md={5} lg={4} xl={3}>
         <SubcategoryGrid />
-        {/* TODO: remove below <pre> */}
-        <pre>
-          (subkategorie nie są pokazywane na Dashboard, tylko na kategoriach)
-        </pre>
       </Grid>
       <Grid item xs={12} md={7} lg={8} xl={9}>
-        {/* <TransactionList /> */}
         <TransactionTable />
       </Grid>
     </Grid>
